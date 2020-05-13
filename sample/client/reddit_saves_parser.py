1379f8f3b952243cfb9c9b0cdec088a4d236366e
Justin Cai <justin.cai101@gmail.com>
2020-05-12 23:39:26 -0700

    feat: jrc - working comments conversion, all comments of single post to one note

--- sample/client/reddit_saves_parser.py
+++ sample/client/reddit_saves_parser.py
@@ -93,7 +93,6 @@ with open("ALL REDDIT SAVES.txt", "r") as f6:
     print len(fileList2)
 
     for idx,link3 in enumerate(fileList2):
-        print link3
         if "reddit" in link3 and len(link3.split('/'))==9:
             posts.append(link3)
         elif "reddit" in link3 and len(link3.split('/'))==10:
@@ -204,5 +203,136 @@ for p,t in enumerate(posts):
     time.sleep(random.randint(1,3))
 
 print 'Done! Errors: '+str(len(errors))+ ' '+str(errors) 
+
+commentGroup = []
+for p,t in enumerate(comments):
+    print(t)
+    commentGroup.append(t)
+    if not p==(len(comments)-1) and comments[p].split('/')[:7] == comments[p+1].split('/')[:7]:
+        continue
+    else:
+        try:
+            req = requests.get('https://api.reddit.com/api/info/?id=t3_'+t.split('/')[6], headers = {'User-agent': 'redditEvernoteBot'})
+
+            # If the response was successful, no Exception will be raised
+            req.raise_for_status()
+        except HTTPError as http_err:
+            print(http_err)  # Python 3.6
+        except Exception as err:
+            print(err)  # Python 3.6
+        else:
+            print(str(p)+'/'+str(len(comments))+' Success! '+comments[p])
+            reqData=req.json()
+            note = Types.Note()
+            try:
+                note.tagNames=[reqData['data']['children'][0]['data']['subreddit'], 'reddit', 'reddit_saves_bot', 'reddit_comment']
+            except:
+                print 'non standard format, skipping'
+                errors.append(posts[p])
+                continue
+            selftext=reqData['data']['children'][0]['data']['selftext'] or None
+            cUrl=reqData['data']['children'][0]['data']['url']
+            contentUrl=cUrl if ("reddit" not in cUrl) else None
+            thumbnailUrl=reqData['data']['children'][0]['data']['thumbnail'] or None
+            postUrl='https://reddit.com'+reqData['data']['children'][0]['data']['permalink']
+            postDate=datetime.utcfromtimestamp(reqData['data']['children'][0]['data']['created_utc']).strftime('%Y-%m-%d')
+            postTitle=(reqData['data']['children'][0]['data']['title'])
+            note.title= u' '.join([postDate, postTitle[:100]]).encode('ascii', 'ignore').decode('ascii').replace('\n', "").strip()
+
+            image=None
+            # Thumbnail can be image url or 'self', 'spoiler', 'default', etc
+            if (thumbnailUrl is not None) and (thumbnailUrl[:4] == 'http'):
+                try:
+                    image = urllib2.urlopen(thumbnailUrl).read()
+                except:
+                    image=None
+
+            if image is not None:
+                md5 = hashlib.md5()
+                md5.update(image)
+                hash = md5.digest()
+
+                data = Types.Data()
+                data.size = len(image)
+                data.bodyHash = hash
+                data.body = image
+
+                resource = Types.Resource()
+                resource.mime = 'image/png'
+                resource.data = data
+
+                # Now, add the new Resource to the note's list of resources
+                note.resources = [resource]
+
+                # To display the Resource as part of the note's content, include an <en-media>
+                # tag in the note's ENML content. The en-media tag identifies the corresponding
+                # Resource using the MD5 hash.
+                hash_hex = binascii.hexlify(hash)
+
+            # The content of an Evernote note is represented using Evernote Markup Language
+            # (ENML). The full ENML specification can be found in the Evernote API Overview
+            # at http://dev.evernote.com/documentation/cloud/chapters/ENML.php
+
+            note.content = '<?xml version="1.0" encoding="UTF-8"?>'
+            note.content += '<!DOCTYPE en-note SYSTEM ' \
+                '"http://xml.evernote.com/pub/enml2.dtd">'
+            note.content += '<en-note><br/>'
+            if selftext is not None:
+                note.content += '<h5>'+selftext+'</h5><br/>'
+            if contentUrl is not None:
+                note.content += '<a href="'+contentUrl+'">'+contentUrl+'</a><br/>'
+            note.content += '<a href="'+postUrl+'">'+postUrl+'</a><br/>'
+            if image is not None: 
+                note.content += '<en-media width="300" height="225" type="image/png" hash="' + hash_hex + '"/>'
+            
+            print commentGroup
+            for i,j in enumerate(commentGroup):
+                try:
+                    req = requests.get('https://api.reddit.com/api/info/?id=t1_'+commentGroup[i].split('/')[8], headers = {'User-agent': 'redditEvernoteBot'})
+
+                    # If the response was successful, no Exception will be raised
+                    req.raise_for_status()
+                except HTTPError as http_err:
+                    print(http_err)  # Python 3.6
+                except Exception as err:
+                    print(err)  # Python 3.6
+                else:
+                    reqData=req.json()
+                    commentBody=None
+                    try:
+                        commentBody=reqData['data']['children'][0]['data']['body']
+                    except:
+                        print 'non standard format, skipping'
+                        continue
+                    print commentBody
+                    note.content += '<br></br>Comment --- '+commentBody+'<br></br>'
+            
+            note.content += '</en-note>'
+
+            # Finally, send the new note to Evernote using the createNote method
+            # The new Note object that is returned will contain server-generated
+            # attributes such as the new note's unique GUID.
+            try:
+                created_note = note_store.createNote(note)
+            except Errors.EDAMSystemException, e:
+                if e.errorCode == Errors.EDAMErrorCode.RATE_LIMIT_REACHED:
+                    print "Rate limit reached"
+                    print "Retry your request in %d seconds" % e.rateLimitDuration
+                    time.sleep(e.rateLimitDuration+1)
+                    try:
+                        created_note = note_store.createNote(note)
+                    except:
+                        print 'failed'
+                        continue
+                        
+                    
+
+        
+        # wait a random amnt of time
+        time.sleep(random.randint(1,3))
+        print 'Done'
+        commentGroup=[]
+
+
 print (len(comments))
 print (len(posts))
